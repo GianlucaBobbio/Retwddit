b88d85382351c70e0cb587e3daad56a42baadada
Gianluca Bobbio <gbobbio@tcn.com.ar>
2015-07-17 15:21:28 -0300

    commit inicial, funciona al recien cargar twitter pero faltaria mejorar al cambiar de pestaña o cargar mas

---
+++ reddIt.js
@@ -0,0 +1,38 @@
+// Checking page title
+if (document.title.indexOf("Twitter") != -1) {
+    //Creating Elements
+    // var btn = document.createElement("BUTTON")
+    // var t = document.createTextNode("CLICK ME");
+    // btn.appendChild(t);
+    //Appending to DOM 
+    // document.body.appendChild(btn);
+
+    var index;
+    var tweets = document.getElementById("stream-items-id").getElementsByClassName("stream-item");
+    for (index = 0; index < tweets.length; ++index) {
+
+    	var tweet = tweets[index];
+    	var fullname = tweet.getElementsByClassName("fullname")[0].innerText;
+    	var tweetText = tweet.getElementsByClassName("tweet-text")[0].innerText;
+		var url = tweet.getElementsByClassName("js-permalink")[0].href;
+
+    	var btn = document.createElement("BUTTON");
+		var t = document.createTextNode("R");
+		btn.className = "button-reddIt";
+		btn.appendChild(t);
+    	btn.type = "button";
+    	btn.fullname = "[" + fullname + "]";
+    	btn.tweetText = tweetText;
+    	btn.text = btn.fullname + " " + btn.tweetText;
+    	btn.url = url;
+    	btn.onclick = copyToClipboard;
+		tweet.getElementsByClassName("time")[0].appendChild(btn);
+	}
+}
+
+function copyToClipboard(event) {
+	var text = event.target.text;
+	var url = event.target.url;
+	window.prompt("Copy to the Title", text);
+	window.prompt("Copy to the Url", url);
+}
