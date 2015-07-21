366d76bd05f07d8ae9f746ed2a1a2d7252889a96
Gianluca Bobbio <gbobbio@tcn.com.ar>
2015-07-21 13:25:17 -0300

    plugin terminado con nombre cambiado

---
+++ retwddit.js
@@ -0,0 +1,45 @@
+// Checking page title
+if (document.title.indexOf("Twitter") != -1) {
+    main();
+}
+
+function main() {
+    var index;
+    var tweets = document.getElementById("stream-items-id").getElementsByClassName("stream-item");
+    for (index = 0; index < tweets.length; ++index) {
+        var tweet = tweets[index];
+        addRedditButton(tweet);
+    }
+
+    $("#stream-items-id").on("DOMNodeInserted", ".tweet.original-tweet.js-stream-tweet", addDinamicallyRedditButton);
+}
+
+function addDinamicallyRedditButton() {
+    var tweet = $(this).get(0);
+    addRedditButton(tweet);
+}
+
+function addRedditButton(argTweet) {
+    var tweet = argTweet;
+    if (tweet.getElementsByClassName("button-retwddit").length < 1) {
+        var fullname = tweet.getElementsByClassName("fullname")[0];
+        var tweetText = tweet.getElementsByClassName("tweet-text")[0];
+        var url = tweet.getElementsByClassName("js-permalink")[0];
+        if (fullname && tweetText && url) {
+            var btn = document.createElement("DIV");
+            btn.className = "button-retwddit";
+            btn.fullname = "[" + fullname.innerText + "]";
+            btn.tweetText = tweetText.innerText;
+            btn.text = btn.fullname + " " + btn.tweetText;
+            btn.url = url.href;
+            btn.onclick = copyToClipboard;
+            tweet.getElementsByClassName("ProfileTweet-actionList")[0].appendChild(btn);
+        }
+    }
+}
+
+function copyToClipboard(event) {
+    var text = event.target.text;
+    var url = event.target.url;
+    window.prompt("Copy to the title and url", text + " " + url);
+}
